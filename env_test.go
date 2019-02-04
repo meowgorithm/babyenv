0546a9718748d15652c3b8ea1c3292c1533e76ec
Christian Rocha <christian@rocha.is>
2019-02-04 11:17:43 -0500

    Genericise test content

--- env_test.go
+++ env_test.go
@@ -8,80 +8,80 @@ import (
 
 func TestParse(t *testing.T) {
 	type config struct {
-		Debug  bool   `env:"DEBUG" default:"false"`
-		DBAddr string `env:"DB_ADDR" default:"localhost:6379"`
-		DBNum  int    `env:"DB_NUM"`
+		A bool   `env:"A"`
+		B string `env:"B"`
+		C int    `env:"C"`
 	}
 
-	debugVal := true
-	dbAddrVal := "example.com:6397"
-	dbNumVal := 16
+	a := true
+	b := "xxx"
+	c := 16
 
-	os.Setenv("DEBUG", strconv.FormatBool(debugVal))
-	os.Setenv("DB_ADDR", dbAddrVal)
-	os.Setenv("DB_NUM", strconv.FormatInt(int64(dbNumVal), 10))
+	os.Setenv("A", strconv.FormatBool(a))
+	os.Setenv("B", b)
+	os.Setenv("C", strconv.FormatInt(int64(c), 10))
 
-	var c config
-	if err := Parse(&c); err != nil {
+	var cfg config
+	if err := Parse(&cfg); err != nil {
 		t.Errorf("error while parsing: %v", err)
 		return
 	}
 
-	if !c.Debug {
-		t.Errorf("failed parsing bool; expected %#v, got %#v", debugVal, c.Debug)
+	if !cfg.A {
+		t.Errorf("failed parsing bool; expected %#v, got %#v", a, cfg.A)
 	}
-	if c.DBAddr != dbAddrVal {
-		t.Errorf("failed parsing string; expected %#v, got %#v", dbAddrVal, c.DBAddr)
+	if cfg.B != b {
+		t.Errorf("failed parsing string; expected %#v, got %#v", b, cfg.B)
 	}
-	if c.DBNum != dbNumVal {
-		t.Errorf("failed parsing int; expected %#v, got %#v", dbNumVal, c.DBNum)
+	if cfg.C != c {
+		t.Errorf("failed parsing int; expected %#v, got %#v", c, cfg.C)
 	}
 }
 
 func TestParseWithDefaults(t *testing.T) {
 	type config struct {
-		Debug  bool   `env:"DEBUG" default:"true"`
-		DBAddr string `env:"DB_ADDR" default:"localhost:6379"`
-		DBNum  int    `env:"DB_NUM" default:"16"`
+		A bool   `env:"A" default:"true"`
+		B string `env:"B" default:"xxx"`
+		C int    `env:"C" default:"16"`
 	}
 
-	debugVal := true
-	dbAddrVal := "localhost:6379"
-	dbNumVal := 16
+	a := true
+	b := "xxx"
+	c := 16
 
-	os.Unsetenv("DEBUG")
-	os.Unsetenv("DB_ADDR")
-	os.Unsetenv("DB_NUM")
+	os.Unsetenv("A")
+	os.Unsetenv("B")
+	os.Unsetenv("C")
 
-	var c config
-	if err := Parse(&c); err != nil {
+	var cfg config
+	if err := Parse(&cfg); err != nil {
 		t.Errorf("error while parsing: %v", err)
 		return
 	}
 
-	if c.Debug != debugVal {
-		t.Errorf("failed parsing bool; expected %#v, got %#v", debugVal, c.Debug)
+	if cfg.A != a {
+		t.Errorf("failed parsing bool; expected %#v, got %#v", a, cfg.A)
 	}
-	if c.DBAddr != dbAddrVal {
-		t.Errorf("failed parsing string; expected %#v, got %#v", dbAddrVal, c.DBAddr)
+	if cfg.B != b {
+		t.Errorf("failed parsing string; expected %#v, got %#v", b, cfg.B)
 	}
-	if c.DBNum != dbNumVal {
-		t.Errorf("failed parsing int; expected %#v, got %#v", dbNumVal, c.DBNum)
+	if cfg.C != c {
+		t.Errorf("failed parsing int; expected %#v, got %#v", c, cfg.C)
 	}
 }
 
 func TestParsePointers(t *testing.T) {
 	type config struct {
-		A *string `env:"A"`
-		B *bool   `env:"B"`
+		A *bool   `env:"A"`
+		B *string `env:"B"`
 		C *int    `env:"C"`
 	}
 
-	a := "xxx"
-	b := true
-	c := 1
-	os.Setenv("A", a)
-	os.Setenv("B", strconv.FormatBool(b))
+	a := true
+	b := "xxx"
+	c := 16
+	os.Setenv("A", strconv.FormatBool(a))
+	os.Setenv("B", b)
 	os.Setenv("C", strconv.FormatInt(int64(c), 10))
 
 	var cfg config
@@ -91,15 +91,15 @@ func TestParsePointers(t *testing.T) {
 	}
 
 	if cfg.A == nil {
-		t.Errorf("failed parsing *string; expected %#v, got nil", a)
+		t.Errorf("failed parsing *bool; expected %#v, got nil", a)
 	} else if *cfg.A != a {
-		t.Errorf("failed parsing *string; expected %#v, got %#v", a, *cfg.A)
+		t.Errorf("failed parsing *bool; expected %#v, got %#v", a, *cfg.A)
 	}
 
 	if cfg.B == nil {
-		t.Errorf("failed parsing *bool; expected %#v, got nil", b)
+		t.Errorf("failed parsing *string; expected %#v, got nil", b)
 	} else if *cfg.B != b {
-		t.Errorf("failed parsing *bool; expected %#v, got %#v", b, *cfg.B)
+		t.Errorf("failed parsing *string; expected %#v, got %#v", b, *cfg.B)
 	}
 
 	if cfg.C == nil {
